4eb169c38c4e1ba1131761b6603a1065d39b817c
Tori <torimpham@gmail.com>
2020-04-25 10:30:26 -0700

    second commit

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var client_id = process.env.clientId; // Your client id
 var client_secret = process.env.clientSecret; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
+const PORT = process.env.PORT || 8888;
+
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -147,4 +150,4 @@ app.get('/refresh_token', function(req, res) {
 });
 
 console.log('Listening on 8888');
-app.listen(8888);
+app.listen(PORT);
